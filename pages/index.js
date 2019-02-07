3f27b0e639b977429cdb281abd704d61f44dc729
Jimmie <jimmiebtlr@gmail.com>
2019-02-07 16:15:26 +0000

    Minimal reproduction.

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Paper from "../components/Paper";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+const Page = () => (
+  <main>
+    <CssBaseline />
+    <Paper>Hello World!</Paper>
+  </main>
+);
+
+export default Page;
